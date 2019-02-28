4671f05741babc2dab8f0c623e3f8aa2d6b9a99e
Dan Le <Danhuyle508@gmail.com>
2019-02-27 20:35:56 -0800

    one error but its almost done

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,40 @@
-from flask import render_template
+from flask import render_template, abort, redirect, url_for, request
+from sqlalchemy.exc import DBAPIError, IntegrityError
+from .models import db, City
 from . import app
+import requests
+import json
+import os
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# @app.route('/search', method=['GET'])
-# def city_search_form():
-#     return render_template('/search.html')
+@app.route('/search', methods=['GET', 'POST'])
+def city_search():   
+    if request.method == 'POST':
+        zipcode = request.form.get('zipcode')
 
-# @app.route('/search', method=['POST'])
-# def city_search_results():
-#     render_template()   
-    
+        url = '{}/weather?zip={}&APPID={}'.format(
+            os.environ.get('API_URL'),
+            zipcode,
+            os.environ.get('API_KEY'),
+        )
+
+        res = requests.get(url)
+        data = json.loads(res.text)
+        try:
+            city = City(name=data['name'], zipcode=zipcode)
+            db.session.add(city)
+            db.session.commit()
+        except (DBAPIError, IntegrityError):
+            abort(400)
+        return redirect(url_for('/portfolio')), 200, 'OK'
+
+    return render_template('./weather/search.html'), 200, 'OK' 
+
+@app.route('/portfolio', methods=['GET'])
+def portfolio_page():
+    return render_tempalte('./weather/portfolio.html')       
+
+
